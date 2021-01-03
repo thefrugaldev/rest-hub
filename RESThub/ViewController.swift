a77d1d5457b5b5e1e8f37a145a7f163cc896275f
Clayton <corman.dev@gmail.com>
2021-01-03 13:45:13 -0500

    :zap: initial commit

---
+++ RESThub/ViewController.swift
@@ -0,0 +1,67 @@
+//
+//  ViewController.swift
+//  RESThub
+//
+//  Created by Clayton Orman on 1/3/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    // MARK: Outlets
+    @IBOutlet weak var feedTableView: UITableView!
+    
+    // MARK: Variables
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func createNewGist(_ sender: UIButton) {
+    }
+    
+    // MARK: Utilities
+    func showResultAlert(title: String, message: String) {
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        alertController.addAction(UIAlertAction(title: "Dismiss", style: .default))
+        
+        self.present(alertController, animated: true, completion: nil)
+    }
+}
+
+// MARK: UITableView Delegate & DataSource
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "feedCellID", for: indexPath)
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+        let starAction = UIContextualAction(style: .normal, title: "Star") { (action, view, completion) in
+            
+            // TODO: PUT a gist star
+            completion(true)
+        }
+        
+        let unstarAction = UIContextualAction(style: .normal, title: "Unstar") { (action, view, completion) in
+            
+            // TODO: DELETE a gist star
+            completion(true)
+        }
+        
+        starAction.backgroundColor = .blue
+        unstarAction.backgroundColor = .darkGray
+        
+        let actionConfig = UISwipeActionsConfiguration(actions: [starAction, unstarAction])
+        
+        return actionConfig
+    }
+}
